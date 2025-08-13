35fd5d48edbc3d02e49e734f5030afc40faef011
Paul Marshall <ptm@esarks.com>
2025-08-12 20:58:01 -0400

    Initial

---
+++ AllowanceAlleyTests/AllowanceAlleyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllowanceAlleyTests.swift
+//  AllowanceAlleyTests
+//
+//  Created by Paul Marshall on 8/12/25.
+//
+
+import Testing
+@testable import AllowanceAlley
+
+struct AllowanceAlleyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
